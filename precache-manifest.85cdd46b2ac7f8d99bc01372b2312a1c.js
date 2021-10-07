404b63c113b41cc7501d5330ee8029158456e3ec
seriksk1 <seriksk1@ukr.net>
2021-10-07 16:59:01 +0300

    Updates

---
+++ precache-manifest.85cdd46b2ac7f8d99bc01372b2312a1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2ab2b18208bfeb905ee31633158f02",
+    "url": "/challenge-mini-game/index.html"
+  },
+  {
+    "revision": "15b0102048ed280c9179",
+    "url": "/challenge-mini-game/static/css/main.fab4b79a.chunk.css"
+  },
+  {
+    "revision": "0597add46c4565f08d65",
+    "url": "/challenge-mini-game/static/js/2.bb840178.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/challenge-mini-game/static/js/2.bb840178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b0102048ed280c9179",
+    "url": "/challenge-mini-game/static/js/main.5ebe6e74.chunk.js"
+  },
+  {
+    "revision": "37359c79182010ec643c",
+    "url": "/challenge-mini-game/static/js/runtime-main.1d6aadb3.js"
+  },
+  {
+    "revision": "be2c31a19181e1792c60e9afa7d92ba6",
+    "url": "/challenge-mini-game/static/media/archer.be2c31a1.png"
+  },
+  {
+    "revision": "643df87e64ee6835e7c1fa033af103fb",
+    "url": "/challenge-mini-game/static/media/cavalry.643df87e.png"
+  },
+  {
+    "revision": "f2f5053526e9311c8d3a5cefdb16c3d9",
+    "url": "/challenge-mini-game/static/media/spearman.f2f50535.png"
+  }
+]);
